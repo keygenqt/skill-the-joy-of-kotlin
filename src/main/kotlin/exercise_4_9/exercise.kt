e406010f97de1976ded779ba2f87bdfa2b60cd94
keygenqt <keygenqt@gmail.com>
2021-05-31 19:02:23 +0300

    exercise_4_9

--- src/main/kotlin/exercise_4_9/exercise.kt
+++ src/main/kotlin/exercise_4_9/exercise.kt
@@ -1,6 +1,25 @@
 package exercise_4_9
 
-fun run() {
+fun plus(): (Int) -> Int = { x -> x + 1 }
 
+fun myRange(start: Int, end: Int): List<Int> {
+    return mutableListOf(start).apply {
+        while (last() < end-1) add(plus().invoke(last()))
+    }
+}
+
+fun bookRange(start: Int, end: Int): List<Int> {
+    val result = mutableListOf<Int>()
+    var index = start
+    while (index < end) {
+        result.add(index)
+        index++
+    }
+    return result
+}
+
+fun run() {
+    println(myRange(1, 10))
+    println(bookRange(1, 10))
 }
 
